f4fdea654e6525652352171492c1f7bb8baf3c65
jkdrangel <jkdrangel@hotmail.com>
2019-11-21 20:46:29 -0300

    :art: Init folder structure

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import React from 'react'
+import { View } from 'react-native'
+import Home from './app/screens/Home'
 
 const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-};
+    justifyContent: 'center'
+  }
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+    </View>
+  )
+}
---
+++ app/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const styles = {
+  container: {
+  },
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Link7</Text>
+    </View>
+  )
+}
