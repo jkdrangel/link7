1be96a039cb646f329df129f731f4dc8528d71bc
jkdrangel <jkdrangel@hotmail.com>
2019-11-21 21:02:38 -0300

    :sparkles: Add api endpoints

---
+++ app/services/api.js
@@ -0,0 +1,7 @@
+const BASE_API = 'https://5dc05c0f95f4b90014ddc651.mockapi.io/elo7/api/1/products'
+
+const getProductsList = () => fetch(BASE_API)
+
+const searchProducts = (query) => fetch(`${BASE_API}?q=${query}`)
+
+export { getProductsList, searchProducts }
